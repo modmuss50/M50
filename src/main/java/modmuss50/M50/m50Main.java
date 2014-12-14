bcb2062cf200631613fdeea95b35dca448f0d8c9
modmuss50 <m123world@gmail.com>
2014-12-14 21:50:42 +0000

    1/2 finished code

--- src/main/java/modmuss50/M50/m50Main.java
+++ src/main/java/modmuss50/M50/m50Main.java
@@ -1,10 +1,18 @@
 package modmuss50.M50;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class m50Main {
 
 	public static File script;
+
+	public static HashMap<Integer, String> gotos = new HashMap<Integer, String>();
+
+	public static Boolean stopReading = false;
+
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Starting M50");
@@ -16,23 +24,52 @@ public class m50Main {
 		}
 		System.out.println("Will now try to run " + Integer.toString(countLines(script)) + " lines of script");
 
+		readFrom(1);
 
+	}
+
+	public static void readFrom(int startLine) throws IOException {
+		boolean proccess = false;
 		int linenumber = 1;
 		BufferedReader br = new BufferedReader(new FileReader(script));
 		String line;
 		while ((line = br.readLine()) != null) {
-			processLine(line);
+			if(stopReading){
+				return;
+			}
+			if(linenumber == startLine){
+				proccess = true;
+			}
+			if(proccess){
+				if(!processLine(line, linenumber)){
+				return;
+				}
+			}
 			linenumber += 1;
 		}
 		br.close();
 	}
 
 
-	public static void processLine (String line){
+
+	public static boolean processLine (String line, int lineNumber) throws IOException {
 		if(line.startsWith("print")){
 			String message = line.replaceAll("\"", "").replace("print:", "");
 			System.out.println(message);
+		} else if(line.startsWith("place")){
+			String name = line;
+			gotos.put(lineNumber, name.replaceAll("\"", "").replace("place:", ""));
+		} else if(line.startsWith("goto")){
+			String name = line.replaceAll("\"", "").replace("goto:", "");
+			for(Map.Entry<Integer, String> entry : gotos.entrySet()){
+				if(entry.getValue().equals(name)){
+					stopReading = true;
+					readFrom(entry.getKey());
+					
+				}
+			}
 		}
+		return true;
 	}
 
 
